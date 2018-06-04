aae26568f6566476ee35df1f60013553ed495ca4
Georgy Zheyts <gzheyts@gmail.com>
2018-06-04 12:24:12 +0700

    chat service properties from source

--- src/main/java/ru/ssnd/demo/vkchat/config/SpringRootConfig.java
+++ src/main/java/ru/ssnd/demo/vkchat/config/SpringRootConfig.java
@@ -2,9 +2,11 @@ package ru.ssnd.demo.vkchat.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan({"ru.ssnd.demo.vkchat.service"})
+@PropertySource("classpath:chat_service.properties")
 public class SpringRootConfig {
 
 }
