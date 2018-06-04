8ae6f86265e0639772da0feb7b1f8dc70ad3953e
Georgy Zheyts <gzheyts@gmail.com>
2018-06-04 12:25:53 +0700

    setup gson message converter

--- src/main/java/ru/ssnd/demo/vkchat/config/SpringWebConfig.java
+++ src/main/java/ru/ssnd/demo/vkchat/config/SpringWebConfig.java
@@ -1,13 +1,19 @@
 package ru.ssnd.demo.vkchat.config;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+import java.util.List;
 
 @EnableWebMvc
 @Configuration
@@ -34,5 +40,22 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter {
         resolver.setSuffix("");
         resolver.setContentType("text/html;charset=utf-8");
         return resolver;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(createGsonHttpMessageConverter());
+        super.configureMessageConverters(converters);
+    }
+
+    private GsonHttpMessageConverter createGsonHttpMessageConverter() {
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .create();
+
+        GsonHttpMessageConverter gsonConverter = new GsonHttpMessageConverter();
+        gsonConverter.setGson(gson);
+
+        return gsonConverter;
     }
 }
